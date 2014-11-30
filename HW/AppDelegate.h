4eb40ab564b5cfbe3537a94ea382bb75d2b4e212
Zhizhik <zhizhik@iMac-Zhizhik.local>
2014-11-30 08:24:20 -0800

    Initial Commit

---
+++ HW/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HW
+//
+//  Created by Zhizhik on 30.11.14.
+//  Copyright (c) 2014 Zhizhik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HW/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HW
+//
+//  Created by Zhizhik on 30.11.14.
+//  Copyright (c) 2014 Zhizhik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
